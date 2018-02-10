2cafe2246b7c5c976099203235018ead21c49e77
nrique <eesta83@gmail.com>
2018-02-09 19:27:28 -0500

    last changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-before_action :authorize,  only: [:show, :edit, :update, :destroy]
+  before_action :authorize,  only: [:show, :edit, :update, :destroy]
   # GET /users
   # GET /users.json
   def index
@@ -27,28 +27,17 @@ before_action :authorize,  only: [:show, :edit, :update, :destroy]
   def create
 
     user= User.new(user_params)
-    
-      if user.save 
+
+      if user.save
         session[:user_id]= user.id
-         
+
         redirect_to user_path(user.id)
       else
         redirect_to '/signup'
-      end  
+      end
     end
 
 
- 
- 
-  #     if @user.save
-        # format.html { redirect_to @user, notice: 'User was successfully created.' }
-        # format.json { render :show, status: :created, location: @user }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
